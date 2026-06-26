96aaf045141cc15d80d9819e38f2f4e5703081e9
Alessandro <alessandro.chitarrini51@gmail.com>
2026-06-26 16:23:35 +0200

    improve comments for lab 08 [part 2]

--- src/exercises/08/sol/e1/client.c
+++ src/exercises/08/sol/e1/client.c
@@ -1,10 +1,39 @@
+/*
+ * This is part of the eighth session.
+ *
+ * Goals:
+ * - Understand the C10K Problem and server scalability bottlenecks.
+ * - Implement server parallelism using multi-process architecture (fork).
+ * - Implement server parallelism using multi-thread architecture (pthread).
+ *
+ * Exercise 1 - Concurrent echo server
+ *
+ * Client
+ *
+ * The client connects to the server via a TCP socket. Once the connection
+ * is established, it reads a welcome message from the server. It then enters 
+ * a loop where it reads user input from stdin, sends it to the server, and 
+ * waits for the echoed response. If the user inputs the QUIT_COMMAND, 
+ * the client sends it and exits the loop, terminating the connection.
+ *
+ * Server
+ *
+ * The server listens for incoming TCP connections. Through conditional 
+ * compilation flags (-DSERVER_SERIAL, -DSERVER_MPROC, -DSERVER_MTHREAD), 
+ * the server can be compiled to handle connections in three different ways:
+ * 
+ * 1. Serially: One client at a time, queueing others.
+ * 2. Multi-Process: A new child process is forked for each incoming connection.
+ * 3. Multi-Thread: A new detached thread is spawned for each incoming connection.
+ */
+
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <arpa/inet.h>  // htons() and inet_addr()
-#include <netinet/in.h> // struct sockaddr_in
+#include <arpa/inet.h>  /* htons() and inet_addr() */
+#include <netinet/in.h> /* struct sockaddr_in */
 #include <sys/socket.h>
 
 #include "common.h"
@@ -12,20 +41,32 @@
 int main(int argc, char* argv[]) {
     int ret;
 
-    // variables for handling a socket
+    /* Variables for handling the client socket connection */
     int socket_desc;
-    struct sockaddr_in server_addr = {0}; // some fields are required to be filled with 0
+    struct sockaddr_in server_addr = {0}; /* Some fields are required to be filled with 0 */
 
-    // create a socket
+    /*
+     * Create a TCP socket for contacting the server.
+     * - Protocol family: AF_INET (IPv4).
+     * - Socket type: SOCK_STREAM (TCP).
+     * - Default protocol: 0.
+     */
     socket_desc = socket(AF_INET, SOCK_STREAM, 0);
     if(socket_desc < 0) handle_error("Could not create socket");
 
-    // set up parameters for the connection
+    /*
+     * Configure the server address structure.
+     * 1. Set IP address using inet_addr().
+     * 2. Set address family (AF_INET).
+     * 3. Set port using htons() for Network Byte Order.
+     */
     server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDRESS);
     server_addr.sin_family      = AF_INET;
-    server_addr.sin_port        = htons(SERVER_PORT); // don't forget about network byte order!
+    server_addr.sin_port        = htons(SERVER_PORT); /* Network byte order conversion */
 
-    // initiate a connection on the socket
+    /*
+     * Initiate a connection on the socket to the configured server address.
+     */
     ret = connect(socket_desc, (struct sockaddr*) &server_addr, sizeof(struct sockaddr_in));
     if(ret) handle_error("Could not create connection");
 
@@ -35,7 +76,12 @@ int main(int argc, char* argv[]) {
     size_t buf_len = sizeof(buf);
     int msg_len;
 
-    // display welcome message from server
+    /*
+     * Receive and display the initial welcome message from the server.
+     *
+     * recv() with flags = 0 is equivalent to read() on a descriptor.
+     * The call blocks until data becomes available.
+     */
     while ( (msg_len = recv(socket_desc, buf, buf_len - 1, 0)) < 0 ) {
         if (errno == EINTR) continue;
         handle_error("Cannot read from socket");
@@ -43,46 +89,59 @@ int main(int argc, char* argv[]) {
     buf[msg_len] = '\0';
     printf("%s", buf);
 
-    // main loop
+    /* Main loop */
     while (1) {
         char* quit_command = SERVER_COMMAND;
         size_t quit_command_len = strlen(quit_command);
 
         printf("Insert your message: ");
 
-        /* Read a line from stdin
-         *
+        /*
+         * Read a line from stdin
+         * 
          * fgets() reads up to sizeof(buf)-1 bytes and on success
-         * returns the first argument passed to it. */
+         * returns the first argument passed to it. 
+         */
         if (fgets(buf, sizeof(buf), stdin) != (char*)buf) {
             fprintf(stderr, "Error while reading from stdin, exiting...\n");
             exit(EXIT_FAILURE);
         }
 
         msg_len = strlen(buf);
-        buf[msg_len-1] = '\n'; // place '\n' from the end of the message
+        buf[msg_len-1] = '\n'; /* Place '\n' at the end of the message */
 
-        // send message to server
-        while ( (ret = send(socket_desc, buf, msg_len, 0)) < 0) {
+        /*
+         * Send the user's message to the server.
+         *
+         * send() with flags = 0 is equivalent to write() on a descriptor.
+         */
+        while ((ret = send(socket_desc, buf, msg_len, 0)) < 0) {
             if (errno == EINTR) continue;
             handle_error("Cannot write to socket");
         }
 
-        /* After a quit command we won't receive any more data from
-         * the server, thus we must exit the main loop. */
+        /*
+         * After a quit command, no more data will be received from
+         * the server, thus the main loop must be exited. 
+         */
         if (msg_len == quit_command_len && !memcmp(buf, quit_command, quit_command_len)) break;
 
-        // read message from server
+        /*
+         * Read the server's response.
+         *
+         * recv() with flags = 0 is equivalent to read() on a descriptor.
+         */
         while ( (msg_len = recv(socket_desc, buf, buf_len, 0)) < 0 ) {
             if (errno == EINTR) continue;
             handle_error("Cannot read from socket");
         }
 
-        printf("Server response: %s\n", buf); // no need to insert '\0'
+        printf("Server response: %s\n", buf); /* No need to insert '\0' */
     }
 
-
-    // close the socket
+    /*
+     * Close socket and release unused resources.
+     */
     ret = close(socket_desc);
     if(ret) handle_error("Cannot close socket");
 
--- src/exercises/08/sol/e1/server.c
+++ src/exercises/08/sol/e1/server.c
@@ -1,21 +1,54 @@
+/*
+ * This is part of the eighth session.
+ *
+ * Goals:
+ * - Understand the C10K Problem and server scalability bottlenecks.
+ * - Implement Server Parallelism using Multi-Process architecture (fork).
+ * - Implement Server Parallelism using Multi-Thread architecture (pthread).
+ *
+ * Exercise 1 - Concurrent echo server
+ *
+ * Client
+ *
+ * The client connects to the server via a TCP socket. Once the connection
+ * is established, it reads a welcome message from the server. It then enters 
+ * a loop where it reads user input from stdin, sends it to the server, and 
+ * waits for the echoed response. If the user inputs the QUIT_COMMAND, 
+ * the client sends it and exits the loop, terminating the connection.
+ *
+ * Server
+ *
+ * The server listens for incoming TCP connections. Through conditional 
+ * compilation flags (-DSERVER_SERIAL, -DSERVER_MPROC, -DSERVER_MTHREAD), 
+ * the server can be compiled to handle connections in three different ways:
+ * 
+ * 1. Serially: One client at a time, queueing others.
+ * 2. Multi-Process: A new child process is forked for each incoming connection.
+ * 3. Multi-Thread: A new detached thread is spawned for each incoming connection.
+ */
+
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
-#include <arpa/inet.h>  // htons()
-#include <netinet/in.h> // struct sockaddr_in
+#include <arpa/inet.h>  /* htons() */
+#include <netinet/in.h> /* struct sockaddr_in */
 #include <sys/socket.h>
 
 #include "common.h"
 
 #ifdef SERVER_SERIAL
-// nothing to do here
+/* Nothing to do here */
 #elif SERVER_MPROC
-// nothing to do here
+/* Nothing to do here */
 #elif SERVER_MTHREAD
 #include <pthread.h>
+/*
+ * Fields for the arguments that will be populated in the main thread 
+ * and then accessed in thread_connection_handler().
+ */
 typedef struct handler_args_s
 {
     int socket_desc;
@@ -33,18 +66,18 @@ void connection_handler(int socket_desc, struct sockaddr_in* client_addr) {
     char* quit_command = SERVER_COMMAND;
     size_t quit_command_len = strlen(quit_command);
 
-    // Parse the ip address of the client and the port number of the client
+    /* Parse the ip address of the client and the port number of the client */
     char client_ip[INET_ADDRSTRLEN];
     inet_ntop(AF_INET, &(client_addr->sin_addr), client_ip, INET_ADDRSTRLEN);
     uint16_t client_port = ntohs(client_addr->sin_port);
     
-    // Send welcome message
+    /* Send welcome message */
     sprintf(buf, "Hi! I'm an echo server. You are %s talking on port %hu.\nI will send you back whatever"
             " you send me. I will stop if you send me %s :-)\n", client_ip, client_port, quit_command);
     msg_len = strlen(buf);
     
     int bytes_sent = 0;
-	while (bytes_sent < msg_len) {
+    while (bytes_sent < msg_len) {
         ret = send(socket_desc, buf + bytes_sent, msg_len - bytes_sent, 0);
         if (ret == -1 && errno == EINTR) continue;
         if (ret == -1) handle_error("Cannot write to the socket");
@@ -52,7 +85,7 @@ void connection_handler(int socket_desc, struct sockaddr_in* client_addr) {
     }
 
     while (1) {
-        // read message from client
+        /* Read message from client */
         memset(buf, 0, buf_len);
         recv_bytes = 0;
         do {
@@ -63,8 +96,11 @@ void connection_handler(int socket_desc, struct sockaddr_in* client_addr) {
         } while ( buf[recv_bytes++] != '\n' );
 
         if (recv_bytes == 0) break;
+        
+        /* Check if it is the quit message, in that case exit from the loop */
         if (recv_bytes == quit_command_len && !memcmp(buf, quit_command, quit_command_len)) break;
 
+        /* Otherwise send back to the client the received message */
         bytes_sent = 0;
         while (bytes_sent < recv_bytes) {
             ret = send(socket_desc, buf + bytes_sent, recv_bytes - bytes_sent, 0);
@@ -74,7 +110,7 @@ void connection_handler(int socket_desc, struct sockaddr_in* client_addr) {
         }
     }
 
-    // close socket
+    /* Close socket */
     ret = close(socket_desc);
     if(ret) handle_error("Cannot close socket for incoming connection");
 }
@@ -82,25 +118,27 @@ void connection_handler(int socket_desc, struct sockaddr_in* client_addr) {
 #ifdef SERVER_SERIAL
 
 void serialServer(int server_desc) {
-    // we initialize client_addr to zero
+    /* Initialize client_addr to zero */
     struct sockaddr_in client_addr = {0};
 
-    // loop to handle incoming connections serially
+    /* Loop to handle incoming connections serially */
     int sockaddr_len = sizeof(struct sockaddr_in);
     while (1) {
         int client_desc = accept(server_desc, (struct sockaddr*) &client_addr, (socklen_t*) &sockaddr_len);
-        if(client_desc == -1 && errno == EINTR) continue; // check for interruption by signals
+        if(client_desc == -1 && errno == EINTR) continue; /* Check for interruption by signals */
         if(client_desc < 0) handle_error("Cannot open socket for incoming connection");
 
         if (DEBUG) fprintf(stderr, "Incoming connection accepted...\n");
 
-        /* We pass the socket descriptor and the address information
-         * for the incoming connection to the handler. */
+        /*
+         * Pass the socket descriptor and the address information
+         * for the incoming connection to the handler. 
+         */
         connection_handler(client_desc, &client_addr);
 
         if (DEBUG) fprintf(stderr, "Done!\n");
 
-        // reset fields in client_addr
+        /* Reset fields in client_addr */
         memset(&client_addr, 0, sizeof(struct sockaddr_in));
     }
 }
@@ -109,47 +147,57 @@ void serialServer(int server_desc) {
 
 void mprocServer(int server_desc) {
     int ret = 0;
-    // we initialize client_addr to zero
+    /* Initialize client_addr to zero */
     struct sockaddr_in client_addr = {0};
 
-    // loop to manage incoming connections forking the server process
+    /* Loop to manage incoming connections forking the server process */
     int sockaddr_len = sizeof(struct sockaddr_in);
     while (1) {
-        // accept incoming connection
+        /* Accept incoming connection */
         int client_desc = accept(server_desc, (struct sockaddr *) &client_addr, (socklen_t *)&sockaddr_len);
-        // check for interruption by signals
+        
+        /* Check for interruption by signals */
         if(client_desc == -1 && errno == EINTR) continue; 
         if(client_desc < 0) handle_error("Cannot open socket for incoming connection");
 
         if (DEBUG) fprintf(stderr, "Incoming connection accepted...\n");
         
+        /*
+         * - Use fork() to create a child process to handle the request.
+         * - Close descriptors that are not used in the parent and the child process.
+         * Note that a connection is closed only when all the descriptors associated 
+         * with it have been closed.
+         * - connection_handler() is executed by the child process.
+         * - memset() is performed in the parent to accept a new request.
+         * - Added debug messages in parent and child.
+         */
         pid_t pid = fork();
         if (pid < 0) {
             handle_error("Failed to fork server process for incoming connection");
         } 
         else if (pid == 0) {
-            // CHILD PROCESS
-            // Close the listening socket; child doesn't accept new connections
+            /* CHILD PROCESS */
+            /* Close the listening socket; child doesn't accept new connections */
             ret = close(server_desc);
             if (ret) handle_error("Cannot close listening socket in child process");
 
-            // Handle the client connection
+            /* Handle the client connection */
             connection_handler(client_desc, &client_addr);
 
             if (DEBUG) fprintf(stderr, "Child process has finished handling the request.\n");
 
-            // Exit child process
+            /* Exit child process */
             _exit(EXIT_SUCCESS);
         } 
         else {
-            // PARENT PROCESS
-            // Close the connected socket; parent doesn't handle this client
+            /* PARENT PROCESS */
+            /* Close the connected socket; parent doesn't handle this client */
             ret = close(client_desc);
             if (ret) handle_error("Cannot close client socket in parent process");
 
             if (DEBUG) fprintf(stderr, "Child process created to handle the request.\n");
 
-            // Reset client address to accept new connections
+            /* Reset client address to accept new connections */
             memset(&client_addr, 0, sizeof(struct sockaddr_in));
         }
     }
@@ -157,35 +205,54 @@ void mprocServer(int server_desc) {
 
 #elif SERVER_MTHREAD
 
+/*
+ * Wrapper method to call connection_handler from the thread.
+ *
+ * - Uses the struct handler_args_t to wrap the arguments.
+ * - After calling connection_handler, frees all the resources used by the thread
+ * and explicitly calls pthread_exit().
+ */
 void *thread_connection_handler(void *arg) {
 
     handler_args_t *args = (handler_args_t *)arg;
     int socket_desc = args->socket_desc;
     struct sockaddr_in *client_addr = args->client_addr;
-    connection_handler(socket_desc,client_addr);
+    
+    connection_handler(socket_desc, client_addr);
 
     free(args->client_addr);
     free(args);
+    
     pthread_exit(NULL);
 }
 
 void mthreadServer(int server_desc) {
     int ret = 0;
-    // we allocate client_addr dynamically and initialize it to zero
+    
+    /* Allocate client_addr dynamically and initialize it to zero */
     struct sockaddr_in* client_addr = calloc(1, sizeof(struct sockaddr_in));
 
-    // loop to manage incoming connections spawning handler threads
+    /* Loop to manage incoming connections spawning handler threads */
     int sockaddr_len = sizeof(struct sockaddr_in);
     while (1) {
-        // accept incoming connection
+        /* Accept incoming connection */
         int client_desc = accept(server_desc, (struct sockaddr*) client_addr, (socklen_t*) &sockaddr_len);
-        if(client_desc == -1 && errno == EINTR) continue; // check for interruption by signals
+        if(client_desc == -1 && errno == EINTR) continue; /* Check for interruption by signals */
         if(client_desc < 0) handle_error("Cannot open socket for incoming connection");
 
         if (DEBUG) fprintf(stderr, "Incoming connection accepted...\n");
 
         pthread_t thread;
 
+        /*
+         * - thread_connection_handler() is executed in the spawned thread.
+         * - A handler_args_t data structure is allocated and populated with arguments.
+         * - Since pthread_join() is not needed, pthread_detach() is used to release 
+         * libpthread's internal resources.
+         * - To safely accept a new connection while a thread is executing, client_addr 
+         * is allocated dynamically inside the loop for future connections. Resetting 
+         * fields in a shared client_addr would cause data races!
+         */
         handler_args_t *thread_args = malloc(sizeof(handler_args_t));
         thread_args->socket_desc = client_desc;
         thread_args->client_addr = client_addr;
@@ -197,6 +264,9 @@ void mthreadServer(int server_desc) {
         
         ret = pthread_detach(thread);
         if (ret) handle_error_en(ret, "Could not detach the thread");
+        
+        /* Allocate a fresh client_addr for the next incoming connection */
+        client_addr = calloc(1, sizeof(struct sockaddr_in));
     }
 }
 
@@ -204,33 +274,32 @@ void mthreadServer(int server_desc) {
 
 int main(int argc, char* argv[]) {
     int ret;
-
     int socket_desc;
 
-    // some fields are required to be filled with 0
+    /* Some fields are required to be filled with 0 */
     struct sockaddr_in server_addr = {0};
 
-     // we will reuse it for accept()
-
-    // initialize socket for listening
+    /* Initialize socket for listening */
     socket_desc = socket(AF_INET , SOCK_STREAM , 0);
     if(socket_desc < 0) handle_error("Could not create socket");
 
-    server_addr.sin_addr.s_addr = INADDR_ANY; // we want to accept connections from any interface
+    server_addr.sin_addr.s_addr = INADDR_ANY; /* Accept connections from any interface */
     server_addr.sin_family      = AF_INET;
-    server_addr.sin_port        = htons(SERVER_PORT); // don't forget about network byte order!
+    server_addr.sin_port        = htons(SERVER_PORT); /* Network byte order conversion */
 
-    /* We enable SO_REUSEADDR to quickly restart our server after a crash:
-     * for more details, read about the TIME_WAIT state in the TCP protocol */
+    /*
+     * Enable SO_REUSEADDR to quickly restart the server after a crash:
+     * for more details, read about the TIME_WAIT state in the TCP protocol.
+     */
     int reuseaddr_opt = 1;
     ret = setsockopt(socket_desc, SOL_SOCKET, SO_REUSEADDR, &reuseaddr_opt, sizeof(reuseaddr_opt));
     if(ret) handle_error("Cannot set SO_REUSEADDR option");
 
-    // bind address to socket
+    /* Bind address to socket */
     ret = bind(socket_desc, (struct sockaddr*) &server_addr, sizeof(struct sockaddr_in));
     if(ret) handle_error("Cannot bind address to socket");
 
-    // start listening
+    /* Start listening */
     ret = listen(socket_desc, MAX_CONN_QUEUE);
     if(ret) handle_error("Cannot listen on socket");
 
@@ -242,5 +311,5 @@ int main(int argc, char* argv[]) {
     serialServer(socket_desc);
 #endif
 
-    exit(EXIT_SUCCESS); // this will never be executed
+    exit(EXIT_SUCCESS); /* This will never be executed */
 }
