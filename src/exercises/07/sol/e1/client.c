fb9061bb9882d1930d7a092fcc9298c00d55fe8f
Alessandro <alessandro.chitarrini51@gmail.com>
2026-06-26 15:59:29 +0200

    improve comments for lab 07 [part 2]

--- src/exercises/07/sol/e1/client.c
+++ src/exercises/07/sol/e1/client.c
@@ -1,26 +1,54 @@
+/*
+ * This is part of the seventh session.
+ *
+ * Goals:
+ * - understand the difference between network byte order and host byte order
+ * - set up a client/server application using TCP and UDP sockets
+ * - implement a simple communication protocol based on text messages
+ *
+ * Exercise 1 - TCP echo server
+ *
+ * Client
+ *
+ * The client connects to the server via a TCP socket. Once the connection
+ * is established, it receives a welcome message. Then, it enters a loop where
+ * it reads user input from stdin, sends it to the server, and waits for the 
+ * echoed response. If the user inputs the QUIT_COMMAND, the client sends it 
+ * and exits the loop, terminating the connection.
+ *
+ * Server
+ *
+ * The server listens for incoming TCP connections on a specific port.
+ * When a client connects, the server sends a welcome message. Then, it enters
+ * an echo loop: it reads incoming text messages from the client and sends the
+ * exact same messages back. If the received message matches the QUIT_COMMAND,
+ * the server breaks the loop, closes the connection with that client, and 
+ * waits for the next incoming connection.
+ */
+
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <arpa/inet.h>  // For htons() and inet_addr()
-#include <netinet/in.h> // For struct sockaddr_in
-#include <sys/socket.h> // For socket API calls
+#include <arpa/inet.h>  /* For htons() and inet_addr() */
+#include <netinet/in.h> /* For struct sockaddr_in */
+#include <sys/socket.h> /* For socket API calls */
 
 #include "common.h"
 
 int main(int argc, char* argv[]) {
     int ret, bytes_sent, recv_bytes;
 
-    // Variables for handling the client socket connection
+    /* Variables for handling the client socket connection */
     int socket_desc;
-    struct sockaddr_in server_addr = {0}; // Initialize to zero
+    struct sockaddr_in server_addr = {0}; /* Initialize to zero */
 
     /*
      * Create a TCP socket for contacting the server.
-     * - AF_INET: IPv4 protocol family.
-     * - SOCK_STREAM: TCP for a reliable, stream-based connection.
-     * - 0: Default protocol (TCP).
+     * - Protocol family: AF_INET (IPv4).
+     * - Socket type: SOCK_STREAM (TCP for a reliable, stream-based connection).
+     * - Default protocol: 0.
      */
     socket_desc = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -33,14 +61,16 @@ int main(int argc, char* argv[]) {
      * 1. Set IP address using inet_addr().
      * 2. Set address family (AF_INET).
      * 3. Set port using htons() for Network Byte Order.
+     *
      * The connect() call attempts the TCP handshake.
-     * A cast to (struct sockaddr*) is required for the connection call.
+     * Attention to the connect method, it requires the second field to be a 
+     * struct sockaddr*, so the struct sockaddr_in must be explicitly cast.
      */
-    server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDRESS); // Convert IP string to binary format
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDRESS); /* Convert IP string to binary format */
     server_addr.sin_family      = AF_INET;
-    server_addr.sin_port        = htons(SERVER_PORT); // Convert port to network byte order
+    server_addr.sin_port        = htons(SERVER_PORT); /* Convert port to network byte order */
 
-    // Initiate a connection on the socket to the configured server address
+    /* Initiate a connection on the socket to the configured server address */
     ret = connect(socket_desc, (struct sockaddr*) &server_addr, sizeof(struct sockaddr_in));
     if (ret < 0) handle_error("Could not create connection");
 
@@ -53,26 +83,28 @@ int main(int argc, char* argv[]) {
 
     /*
      * Receive and display the initial welcome message from the server.
-     * Use a loop with recv() to handle potentially fragmented messages.
-     * Reading continues until a newline ('\n') is received or the peer closes.
-     * Error handling checks for EINTR (interruption), general error (-1),
-     * and peer closing the connection (0).
+     * 
+     * - recv() with flags = 0 is equivalent to read() on a descriptor.
+     * - A loop is used to deal with partially received messages.
+     * - For sockets, a 0 return value is received only when the peer closes 
+     * the connection. If there are no bytes to read, recv() blocks.
+     * - Error handling checks for EINTR (interruption).
      */
     recv_bytes = 0;
     do {
-        // Read data into the buffer, starting from the position of the last read byte
+        /* Read data into the buffer, starting from the position of the last read byte */
         ret = recv(socket_desc, buf + recv_bytes, buf_len - recv_bytes, 0);
         if (ret == -1 && errno == EINTR) continue;
         if (ret == -1) handle_error("Cannot read from the socket");
-        if (ret == 0) break; // Connection closed by peer
+        if (ret == 0) break; /* Connection closed by peer */
         recv_bytes += ret;
 
-    } while (buf[recv_bytes - 1] != '\n'); // Keep reading until the newline character is found
+    } while (buf[recv_bytes - 1] != '\n'); /* Keep reading until the newline character is found */
     printf("%s", buf);
 
     if (DEBUG) fprintf(stderr, "Received message of %d bytes...\n", recv_bytes);
 
-    // main communication loop
+    /* Main communication loop */
     while (1) {
         char* quit_command = SERVER_COMMAND;
         size_t quit_command_len = strlen(quit_command);
@@ -92,48 +124,53 @@ int main(int argc, char* argv[]) {
 
         /*
          * Send the user's message to the server.
-         * Use a loop with send() to handle partial writes and ensure the entire 
-         * message (up to msg_len) is transmitted.
-         * send() with flags = 0 is equivalent to write().
+         *
+         * - send() with flags = 0 is equivalent to write() on a descriptor.
+         * - A loop is used to deal with partially sent messages.
+         * - The size of the message is specifically msg_len, not the total buffer size.
          */
         bytes_sent = 0;
         while (bytes_sent < msg_len) {
-            // Send the remaining part of the message
+            /* Send the remaining part of the message */
             ret = send(socket_desc, buf + bytes_sent, msg_len - bytes_sent, 0);
-            if (ret == -1 && errno == EINTR) continue; // Retry on interruption
+            if (ret == -1 && errno == EINTR) continue; /* Retry on interruption */
             if (ret == -1) handle_error("Cannot write to the socket");
-            bytes_sent += ret; // Update the count of bytes successfully sent
+            bytes_sent += ret; /* Update the count of bytes successfully sent */
         }
 
         if (DEBUG) fprintf(stderr, "Sent message of %d bytes...\n", bytes_sent);
 
         /*
          * Check if the sent message was the QUIT command.
-         * * If the message matches SERVER_COMMAND, the server will close the
-         * connection, so the client must break the loop. memcmp() is used
-         * for byte-by-byte comparison.
+         *
+         * - Compare the number of bytes sent with the length of the quit command.
+         * - Perform a byte-to-byte comparison using memcmp().
+         * - Break the cycle when the connection is intended to be closed.
          */
         if (msg_len == quit_command_len && !memcmp(buf, quit_command, quit_command_len)) {
 
             if (DEBUG) fprintf(stderr, "Sent QUIT command ...\n");
-            break; // Exit the main loop
+            break; /* Exit the main loop */
         }
 
         /*
          * Read the server's response.
-         * Loop with recv() to read the full response until a newline ('\n') 
-         * is received or the server closes the connection (ret == 0).
+         *
+         * - recv() with flags = 0 is equivalent to read() from a descriptor.
+         * - It handles partially sent messages by continuing to read until 
+         * the newline character is found.
+         * - Checks for ret == 0 to detect if the peer unexpectedly closed the connection.
          */
         recv_bytes = 0;
         do {
-            // Read data into the buffer, starting from the position of the last read byte
+            /* Read data into the buffer, starting from the position of the last read byte */
             ret = recv(socket_desc, buf + recv_bytes, buf_len - recv_bytes, 0);
-            if (ret == -1 && errno == EINTR) continue; // Retry on interruption
+            if (ret == -1 && errno == EINTR) continue; /* Retry on interruption */
             if (ret == -1) handle_error("Cannot read from the socket");
-            if (ret == 0) break; // Connection closed by peer
-            recv_bytes += ret; // Update the count of bytes successfully received
+            if (ret == 0) break; /* Connection closed by peer */
+            recv_bytes += ret; /* Update the count of bytes successfully received */
 
-        } while (buf[recv_bytes - 1] != '\n'); // Keep reading until the newline character is found
+        } while (buf[recv_bytes - 1] != '\n'); /* Keep reading until the newline character is found */
 
         if (DEBUG) fprintf(stderr, "Received answer of %d bytes...\n", recv_bytes);
 
@@ -141,7 +178,7 @@ int main(int argc, char* argv[]) {
     }
 
     /*
-     * Close the socket and release resources.
+     * Close socket and release unused resources.
      */
     ret = close(socket_desc);
     if (ret < 0) handle_error("Cannot close the socket");
--- src/exercises/07/sol/e1/server.c
+++ src/exercises/07/sol/e1/server.c
@@ -1,55 +1,94 @@
+/*
+ * This is part of the seventh session.
+ *
+ * Goals:
+ * - understand the difference between network byte order and host byte order
+ * - set up a client/server application using TCP and UDP sockets
+ * - implement a simple communication protocol based on text messages
+ *
+ * Exercise 1 - TCP echo server
+ *
+ * Client
+ *
+ * The client connects to the server via a TCP socket. Once the connection
+ * is established, it receives a welcome message. Then, it enters a loop where
+ * it reads user input from stdin, sends it to the server, and waits for the 
+ * echoed response. If the user inputs the QUIT_COMMAND, the client sends it 
+ * and exits the loop, terminating the connection.
+ *
+ * Server
+ *
+ * The server listens for incoming TCP connections on a specific port.
+ * When a client connects, the server sends a welcome message. Then, it enters
+ * an echo loop: it reads incoming text messages from the client and sends the
+ * exact same messages back. If the received message matches the QUIT_COMMAND,
+ * the server breaks the loop, closes the connection with that client, and 
+ * waits for the next incoming connection.
+ */
+
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
-#include <arpa/inet.h>  // htons()
-#include <netinet/in.h> // struct sockaddr_in
+#include <arpa/inet.h>  /* htons() */
+#include <netinet/in.h> /* struct sockaddr_in */
 #include <sys/socket.h>
 
 #include "common.h"
 
-// Method for processing incoming requests. The method takes as argument
-// the socket descriptor for the incoming connection.
+/*
+ * Connection handler 
+ *
+ * Method for processing incoming requests. The method takes as argument
+ * the socket descriptor for the incoming connection.
+ */
 void* connection_handler(int socket_desc) {
     int ret, recv_bytes, bytes_sent;
 
     char buf[1024];
     size_t buf_len = sizeof(buf);
     int msg_len;
-    memset(buf,0,buf_len);
+    memset(buf, 0, buf_len);
 
     char* quit_command = SERVER_COMMAND;
     size_t quit_command_len = strlen(quit_command);
 
-    // send welcome message
+    /* Prepare welcome message */
     sprintf(buf, "Hi! I'm an echo server. I will send you back whatever"
             " you send me. I will stop if you send me %s", quit_command);
     msg_len = strlen(buf);
     
     /*
-     * Send the welcome message to the client.
-     * The while loop handles partial transmissions to ensure all 'msg_len' bytes 
-     * are reliably sent using the socket descriptor.
+     * Sending welcome message.
+     *
+     * - send() with flags = 0 is equivalent to write() to a descriptor.
+     * - The message has been written in 'buf'.
+     * - Deals with partially sent messages by using a while loop.
+     * - The size used is 'msg_len', not the entire buffer size.
      */
     bytes_sent = 0;
-    while ( bytes_sent < msg_len) {
+    while (bytes_sent < msg_len) {
         ret = send(socket_desc, buf + bytes_sent, msg_len - bytes_sent, 0);
         if (ret == -1 && errno == EINTR) continue;
         if (ret == -1) handle_error("Cannot write to the socket");
         bytes_sent += ret;
     }
 
-    if (DEBUG) fprintf(stderr, "Welcome message <<%s>> has been sent\n",buf);
+    if (DEBUG) fprintf(stderr, "Welcome message <<%s>> has been sent\n", buf);
 
-    // echo loop
+    /* Echo loop */
     while (1) {
         /*
-         * Receive the client's command or message.
-         * The loop reads data one byte at a time until a newline ('\n') is found 
-         * or the connection is closed (ret == 0). This blocks until data arrives.
-         * The received count is stored in 'recv_bytes'.
+         * Receive the command.
+         *
+         * - recv() with flags = 0 is equivalent to read() from a descriptor.
+         * - The number of received bytes is tracked in 'recv_bytes'.
+         * - For sockets, a 0 return value is obtained only when the peer closes 
+         * the connection. If there are no bytes, the blocking call waits.
+         * - Deals with unknown message sizes by reading one byte at a time
+         * until a newline is encountered.
          */
         recv_bytes = 0;
         do {
@@ -57,14 +96,16 @@ void* connection_handler(int socket_desc) {
             if (ret == -1 && errno == EINTR) continue;
             if (ret == -1) handle_error("Cannot read from the socket");
             if (ret == 0) break;
-        } while ( buf[recv_bytes++] != '\n' );
+        } while (buf[recv_bytes++] != '\n');
 
-        if (DEBUG) fprintf(stderr, "Received command of %d bytes...\n",recv_bytes);
+        if (DEBUG) fprintf(stderr, "Received command of %d bytes...\n", recv_bytes);
 
         /*
-         * Check if the received command matches the quit string (SERVER_COMMAND).
-         * If the message length and byte-by-byte comparison match, the server
-         * breaks the echo loop to shut down the connection gracefully.
+         * Check if the quit_command is received. If so, quit from the echo loop.
+         *
+         * - Compare the number of bytes received with the length of the quit command.
+         * - Perform a byte-to-byte comparison using memcmp().
+         * - Exit from the cycle when the quit command is successfully parsed.
          */
         if (recv_bytes == quit_command_len && !memcmp(buf, quit_command, quit_command_len)){
 
@@ -72,14 +113,15 @@ void* connection_handler(int socket_desc) {
              break;
          }
 
-        // ...or I have to send the message back
         /*
          * Echo the received message back to the client.
-         * The loop ensures all 'recv_bytes' are sent, handling potential partial 
-         * writes until the full message is transmitted.
+         *
+         * - send() with flags = 0 is equivalent to write() on a descriptor.
+         * - Deals with partially sent messages via a while loop.
+         * - The transmission size is specifically 'recv_bytes', NOT the buf size.
          */
-        bytes_sent=0;
-        while ( bytes_sent < recv_bytes) {
+        bytes_sent = 0;
+        while (bytes_sent < recv_bytes) {
             ret = send(socket_desc, buf + bytes_sent, recv_bytes - bytes_sent, 0);
             if (ret == -1 && errno == EINTR) continue;
             if (ret == -1) handle_error("Cannot write to the socket");
@@ -89,11 +131,10 @@ void* connection_handler(int socket_desc) {
         if (DEBUG) fprintf(stderr, "Sent message of %d bytes back...\n", bytes_sent);
     }
 
-
     /*
-     * Close the socket descriptor for the client connection and release resources.
+     * Close socket and release unused resources.
      */
-        ret = close(socket_desc);
+    ret = close(socket_desc);
     if (ret < 0) handle_error("Cannot close socket for incoming connection");
 
     if (DEBUG) fprintf(stderr, "Socket closed...\n");
@@ -106,37 +147,45 @@ int main(int argc, char* argv[]) {
     int ret;
     int socket_desc, client_desc;
 
-    // Initialize address structures to zero
+    /* Initialize address structures to zero */
     struct sockaddr_in server_addr = {0}, client_addr = {0};
 
-    int sockaddr_len = sizeof(struct sockaddr_in); // Reused for accept()
+    int sockaddr_len = sizeof(struct sockaddr_in); /* Reused for accept() */
 
     /*
-     * Create a socket for listening for incoming connections.
-     * Uses AF_INET (IPv4) and SOCK_STREAM (TCP).
+     * Create a socket for listening.
+     * 
+     * - Protocol family is AF_INET.
+     * - Socket type is SOCK_STREAM (TCP).
      */
-    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
     if (socket_desc < 0)
         handle_error("Could not create socket");
 
     if (DEBUG) fprintf(stderr, "Socket created...\n");
 
-    /* We enable SO_REUSEADDR to quickly restart our server after a crash:
-     * for more details, read about the TIME_WAIT state in the TCP protocol */
+    /*
+     * Enable SO_REUSEADDR to quickly restart our server after a crash:
+     * for more details, read about the TIME_WAIT state in the TCP protocol 
+     */
     int reuseaddr_opt = 1;
     ret = setsockopt(socket_desc, SOL_SOCKET, SO_REUSEADDR, &reuseaddr_opt, sizeof(reuseaddr_opt));
     if (ret < 0)
         handle_error("Cannot set SO_REUSEADDR option");
 
     /*
-     * Configure server address and bind it to the socket.
-     * Sets IP to INADDR_ANY (accepting connections on all interfaces) and 
-     * converts the port to network byte order using htons().
-     * The bind() function associates the address with the listening socket.
+     * Set server address and bind it to the socket.
+     *
+     * - server_addr.sin_addr.s_addr is set to INADDR_ANY to accept connections 
+     * from any interface.
+     * - server_addr.sin_family is set to AF_INET.
+     * - server_addr.sin_port is configured using the htons() method.
+     * - The bind method requires the second argument to be struct sockaddr*, 
+     * hence the struct sockaddr_in is cast properly.
      */
-    server_addr.sin_addr.s_addr = INADDR_ANY; // we want to accept connections from any interface
+    server_addr.sin_addr.s_addr = INADDR_ANY; 
     server_addr.sin_family      = AF_INET;
-    server_addr.sin_port        = htons(SERVER_PORT); // don't forget about network byte order!
+    server_addr.sin_port        = htons(SERVER_PORT); 
 
     ret = bind(socket_desc, (struct sockaddr*) &server_addr, sockaddr_len);
     if (ret < 0)
@@ -145,9 +194,9 @@ int main(int argc, char* argv[]) {
     if (DEBUG) fprintf(stderr, "Binded address to socket...\n");
 
     /*
-     * Start listening for incoming connections.
-     * The MAX_CONN_QUEUE value sets the maximum number of clients that can wait
-     * in the connection queue.
+     * Start listening.
+     * 
+     * The number of pending connections is set to MAX_CONN_QUEUE.
      */
     ret = listen(socket_desc, MAX_CONN_QUEUE);
     if (ret < 0)
@@ -155,17 +204,20 @@ int main(int argc, char* argv[]) {
 
     if (DEBUG) fprintf(stderr, "Socket is listening...\n");
 
-    // loop to handle incoming connections (sequentially)
+    /* Loop to handle incoming connections sequentially */
     while (1) {
         /*
          * Accept an incoming connection.
-         * accept() blocks until a client attempts to connect, returning a new
-         * socket descriptor ('client_desc') for communication with that client.
-         * The client's address is stored in 'client_addr'.
+         *
+         * - The descriptor returned by accept() is stored in the client_desc variable.
+         * - The address of the client_addr variable is passed, cast to struct sockaddr*.
+         * - The size of the client_addr structure is passed via the sockaddr_len 
+         * variable, cast to socklen_t*.
+         * - The return value of accept() is checked for errors.
          */
         client_desc = accept(socket_desc, (struct sockaddr*) &client_addr, (socklen_t*) &sockaddr_len);
         if (client_desc < 0)
-        handle_error("Cannot open socket for incoming connection");
+            handle_error("Cannot open socket for incoming connection");
 
         if (DEBUG) fprintf(stderr, "Incoming connection accepted...\n");
 
@@ -174,5 +226,5 @@ int main(int argc, char* argv[]) {
         if (DEBUG) fprintf(stderr, "Done!\n");
     }
 
-    exit(EXIT_SUCCESS); // this will never be executed
+    exit(EXIT_SUCCESS); /* This will never be executed */
 }
